5c97a4a284f0e511f4dd18563d5a4d5e930ff7e6
YANG Xudong <xudong.yang@hde.co.jp>
2015-12-17 18:06:55 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat > ~/.pypirc << EOF
+[distutils]
+index-servers = pypi
+
+[pypi]
+repository=https://pypi.python.org/pypi
+username=${PYPI_USERNAME}
+password=${PYPI_PASSWORD}
+EOF
+
+python setup.py sdist upload
