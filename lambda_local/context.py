0fa224ba04043d8c45c858a585c8c1227fd4c7be
YANG Xudong <xudong.yang@hde.co.jp>
2018-11-08 17:17:12 +0900

    Fix context implementation.

--- lambda_local/context.py
+++ lambda_local/context.py
@@ -2,32 +2,46 @@
 Copyright 2015-2018 HDE, Inc.
 Licensed under MIT.
 '''
+from __future__ import print_function
 
 from datetime import datetime
 from datetime import timedelta
+import uuid
 
 
 class Context(object):
-    def __init__(self, timeout, arn_string, version_name):
-        self.function_name = "undefined"
-        self.function_version = version_name
-        self.invoked_function_arn = arn_string
-        self.memory_limit_in_mb = 0
-        self.aws_request_id = "undefined"
-        self.log_group_name = "undefined"
-        self.log_stream_name = "undefined"
-        self.identity = None
-        self.client_context = None
-        self.timeout = timeout
-        self.duration = timedelta(seconds=timeout)
+    def __init__(self, timeout_in_seconds,
+                 aws_request_id=uuid.uuid4(),
+                 function_name="undefined",
+                 function_version="$LATEST",
+                 log_group_name="undefined",
+                 log_stream_name="undefined",
+                 invoked_function_arn="undefined",
+                 memory_limit_in_mb='0',
+                 client_context=None,
+                 identity=None):
+        self.function_name = function_name
+        self.function_version = function_version
+        self.invoked_function_arn = invoked_function_arn
+        self.memory_limit_in_mb = memory_limit_in_mb
+        self.aws_request_id = aws_request_id
+        self.log_group_name = log_group_name
+        self.log_stream_name = log_stream_name
+        self.identity = identity
+        self.client_context = client_context
+
+        self._duration = timedelta(seconds=timeout_in_seconds)
 
     def get_remaining_time_in_millis(self):
-        if self.timelimit is None:
+        if self._timelimit is None:
             raise Exception("Context not activated.")
-        return millis_interval(datetime.now(), self.timelimit)
+        return millis_interval(datetime.now(), self._timelimit)
 
-    def activate(self):
-        self.timelimit = datetime.now() + self.duration
+    def log(self, msg):
+        print(msg)
+
+    def _activate(self):
+        self._timelimit = datetime.now() + self._duration
         return self
 
 
