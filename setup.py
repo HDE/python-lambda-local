7c2583a7e4936a59dbe0883736efc007387a3ac2
Iskandar Setiadi <iskandarsetiadi@gmail.com>
2019-11-12 19:16:33 +0900

    Set specific pytest version for Python 2

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ class PyTest(TestCommand):
 
 version = "0.1.8"
 
+TEST_REQUIRE = ['pytest']
+if sys.version_info[0] == 2:
+    TEST_REQUIRE = ['pytest==4.6.3']
+
 setup(name="python-lambda-local",
       version=version,
       description="Run lambda function in python on local machine.",
@@ -46,7 +50,7 @@ setup(name="python-lambda-local",
       include_package_data=True,
       zip_safe=False,
       python_requires='>=2.7,!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
-      tests_require=['pytest'],
+      tests_require=TEST_REQUIRE,
       cmdclass={'test': PyTest},
       install_requires=['boto3'],
       entry_points={
